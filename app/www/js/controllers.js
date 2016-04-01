3657dca63b1416a49622c012d367153c684f003b
Vinicius Mesquita <viniciusfmesquita@gmail.com>
2016-04-01 13:51:09 -0300

    wip

--- app/www/js/controllers.js
+++ app/www/js/controllers.js
@@ -1,21 +1,22 @@
 angular.module('starter.controllers', [])
 
-.controller('DashCtrl', function($scope, Events) {
+.controller('EventsCtrl', function($scope, Events) {
   $scope.chats = Events.all();
-  $scope.remove = function(chat) {
-    Events.remove(chat);
+  $scope.remove = function(event) {
+    Events.remove(event);
   };
 })
 
-.controller('ChatsCtrl', function($scope, Friends) {
-  // With the new view caching in Ionic, Controllers are only called
-  // when they are recreated or on app start, instead of every page change.
-  // To listen for when this page is active (for example, to refresh data),
-  // listen for the $ionicView.enter event:
-  //
-  //$scope.$on('$ionicView.enter', function(e) {
-  //});
+.controller('EventDetailCtrl', function($scope, $state, $stateParams, Events, Dudes) {
+  $scope.event = Events.get($stateParams.id);
+  $scope.dudes = Dudes.all();
 
+  $scope.scan = function(id) {
+    $state.go('tab.scan');
+  }
+})
+
+.controller('ChatsCtrl', function($scope, Friends) {
   $scope.chats = Friends.all();
   $scope.remove = function(chat) {
     Friends.remove(chat);
@@ -24,10 +25,6 @@ angular.module('starter.controllers', [])
 
 .controller('ChatDetailCtrl', function($scope, $stateParams, Friends) {
   $scope.chat = Friends.get($stateParams.chatId);
-})
-
-.controller('EventDetailCtrl', function($scope, $stateParams, Events) {
-  $scope.chat = Events.get($stateParams.chatId);
 })
 
 .controller('AccountCtrl', function($scope) {
--- app/www/js/services.js
+++ app/www/js/services.js
@@ -14,21 +14,6 @@ angular.module('starter.services', [])
     name: 'Max Lynx',
     lastText: 'Hey, it\'s me',
     face: 'img/max.png'
-  }, {
-    id: 2,
-    name: 'Adam Bradleyson',
-    lastText: 'I should buy a boat',
-    face: 'img/adam.jpg'
-  }, {
-    id: 3,
-    name: 'Perry Governor',
-    lastText: 'Look at my mukluks!',
-    face: 'img/perry.png'
-  }, {
-    id: 4,
-    name: 'Mike Harrington',
-    lastText: 'This is wicked good ice cream.',
-    face: 'img/mike.png'
   }];
 
   return {
@@ -85,5 +70,56 @@ angular.module('starter.services', [])
     }
   };
 
+})
+
+.factory('Dudes', function() {
+
+  var dudes = [{
+    id: 0,
+    name: 'Ben Sparrow',
+    face: 'img/ben.png',
+    style: ''
+  }, {
+    id: 1,
+    name: 'Max Lynx',
+    lastText: 'Hey, it\'s me',
+    face: 'img/max.png',
+    style: ''
+  }, {
+    id: 2,
+    name: 'Adam Bradleyson',
+    lastText: 'I should buy a boat',
+    face: 'img/adam.jpg',
+    style: 'desature'
+  }, {
+    id: 3,
+    name: 'Perry Governor',
+    lastText: 'Look at my mukluks!',
+    face: 'img/perry.png',
+    style: 'desature'
+  }, {
+    id: 4,
+    name: 'Mike Harrington',
+    lastText: 'This is wicked good ice cream.',
+    face: 'img/mike.png',
+    style: 'desature'
+  }];
+
+  return {
+    all: function() {
+      return dudes;
+    },
+    remove: function(dude) {
+      dudes.splice(dudes.indexOf(dude), 1);
+    },
+    get: function(id) {
+      for (var i = 0; i < dudes.length; i++) {
+        if (dudes[i].id === parseInt(id)) {
+          return dudes[i];
+        }
+      }
+      return null;
+    }
+  };
 
 })
