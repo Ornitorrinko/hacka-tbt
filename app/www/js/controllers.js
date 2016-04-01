dafb756653fa6c06a286b74f57dab097026f4a0e
Vinicius Mesquita <viniciusfmesquita@gmail.com>
2016-04-01 12:15:49 -0300

    Some shit.

---
+++ app/www/js/controllers.js
@@ -0,0 +1,33 @@
+angular.module('starter.controllers', [])
+
+.controller('DashCtrl', function($scope, Events) {
+  $scope.chats = Events.all();
+  $scope.remove = function(chat) {
+    Events.remove(chat);
+  };
+})
+
+.controller('ChatsCtrl', function($scope, Friends) {
+  // With the new view caching in Ionic, Controllers are only called
+  // when they are recreated or on app start, instead of every page change.
+  // To listen for when this page is active (for example, to refresh data),
+  // listen for the $ionicView.enter event:
+  //
+  //$scope.$on('$ionicView.enter', function(e) {
+  //});
+
+  $scope.chats = Friends.all();
+  $scope.remove = function(chat) {
+    Friends.remove(chat);
+  };
+})
+
+.controller('ChatDetailCtrl', function($scope, $stateParams, Friends) {
+  $scope.chat = Friends.get($stateParams.chatId);
+})
+
+.controller('AccountCtrl', function($scope) {
+  $scope.settings = {
+    enableFriends: true
+  };
+});
---
+++ app/www/js/services.js
@@ -0,0 +1,89 @@
+angular.module('starter.services', [])
+
+.factory('Friends', function() {
+  // Might use a resource here that returns a JSON array
+
+  // Some fake testing data
+  var friends = [{
+    id: 0,
+    name: 'Ben Sparrow',
+    lastText: 'You on your way?',
+    face: 'img/ben.png'
+  }, {
+    id: 1,
+    name: 'Max Lynx',
+    lastText: 'Hey, it\'s me',
+    face: 'img/max.png'
+  }, {
+    id: 2,
+    name: 'Adam Bradleyson',
+    lastText: 'I should buy a boat',
+    face: 'img/adam.jpg'
+  }, {
+    id: 3,
+    name: 'Perry Governor',
+    lastText: 'Look at my mukluks!',
+    face: 'img/perry.png'
+  }, {
+    id: 4,
+    name: 'Mike Harrington',
+    lastText: 'This is wicked good ice cream.',
+    face: 'img/mike.png'
+  }];
+
+  return {
+    all: function() {
+      return friends;
+    },
+    remove: function(chat) {
+      friends.splice(friends.indexOf(chat), 1);
+    },
+    get: function(chatId) {
+      for (var i = 0; i < friends.length; i++) {
+        if (friends[i].id === parseInt(chatId)) {
+          return friends[i];
+        }
+      }
+      return null;
+    }
+  };
+})
+
+.factory('Events', function() {
+
+  var events = [{
+    id: 0,
+    name: 'AWS Summit 2016',
+    lastText: '28/04/2016',
+    face: 'img/aws.png'
+  }, {
+    id: 1,
+    name: 'HSM Expo 2016',
+    lastText: '25/05/2016',
+    face: 'img/hsm.png'
+  }, {
+    id: 2,
+    name: 'Qcon SP 2016',
+    lastText: '15/06/2016',
+    face: 'img/qcon.jpg'
+  }];
+
+  return {
+    all: function() {
+      return events;
+    },
+    remove: function(chat) {
+      events.splice(events.indexOf(chat), 1);
+    },
+    get: function(chatId) {
+      for (var i = 0; i < events.length; i++) {
+        if (events[i].id === parseInt(chatId)) {
+          return events[i];
+        }
+      }
+      return null;
+    }
+  };
+
+
+})
