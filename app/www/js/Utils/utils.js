52c97640469de9bc2918740ccb6b318dee431bbf
Vinicius Mesquita <viniciusfmesquita@gmail.com>
2016-04-01 14:19:52 -0300

    grmbl

--- app/www/js/Utils/utils.js
+++ app/www/js/Utils/utils.js
@@ -1,20 +1,20 @@
 angular.module('ornito.utils', [])
 
 // Services
-.factory('Utils', function ($ionicLoading,$ionicPopup) {
-  var self =this;
-  self.displayLoading = function (message){
-      return $ionicLoading.show({
-        template: '<ion-spinner icon="bubbles"></ion-spinner><br/>'+ (message || 'Aguarde')
-      });
+.factory('Utils', function($ionicLoading, $ionicPopup) {
+  var self = this;
+  self.displayLoading = function(message) {
+    return $ionicLoading.show({
+      template: '<ion-spinner icon="bubbles"></ion-spinner><br/>' + (message || 'Aguarde')
+    });
   }
-  self.hideLoading = function () {
+  self.hideLoading = function() {
     return $ionicLoading.hide();
   }
   self.showAlert = function(title, message) {
     var alertPopup = $ionicPopup.alert({
-      title: title || 'Ops! Temos um erro inesperado',
-      template: message || 'Porém não se preocupe, nossos doutores em tecnologia já estão cuidando do problema.'
+      title: title
+      template: message
     });
     alertPopup.then(function(res) {});
   }
